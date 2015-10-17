f001fd0a799ad55ed1add0637b37cfdd8c5ee7ca
Mohammad Rahhal <m.r992@hotmail.com>
2015-10-17 09:55:16 +0300

    + IGeneratorOptions.PostString

--- src/ExternalTemplates.AspNet/IGeneratorOptions.Default.cs
+++ src/ExternalTemplates.AspNet/IGeneratorOptions.Default.cs
@@ -1,4 +1,6 @@
-﻿namespace ExternalTemplates
+﻿using System;
+
+namespace ExternalTemplates
 {
 	/// <summary>
 	/// Default generator options.
@@ -6,15 +8,21 @@
 	public class GeneratorOptions : IGeneratorOptions
 	{
 		/// <summary>
+		/// Gets the path relative to the web root where the templates are stored.
+		/// Default is "/Content/templates".
+		/// </summary>
+		public string VirtualPath { get; set; }
+
+		/// <summary>
 		/// Gets the extension of the templates.
 		/// Default is ".tmpl.html".
 		/// </summary>
 		public string Extension { get; set; }
 
 		/// <summary>
-		/// Gets the path relative to the web root where the templates are stored.
-		/// Default is "/Content/templates".
+		/// Gets the post string to add to the end of the script tag's id following its name.
+		/// Default is "-tmpl".
 		/// </summary>
-		public string VirtualPath { get; set; }
+		public string PostString { get; set; }
 	}
 }
--- src/ExternalTemplates.AspNet/IGeneratorOptions.cs
+++ src/ExternalTemplates.AspNet/IGeneratorOptions.cs
@@ -6,13 +6,18 @@
 	public interface IGeneratorOptions
 	{
 		/// <summary>
-		/// Gets the extension of the templates.
-		/// </summary>
-		string Extension { get; }
-
-		/// <summary>
 		/// Gets the path relative to the web root where the templates are stored.
 		/// </summary>
-		string VirtualPath { get; }
+		string VirtualPath { get; set; }
+
+		/// <summary>
+		/// Gets the extension of the templates.
+		/// </summary>
+		string Extension { get; set; }
+
+		/// <summary>
+		/// Gets the post string to add to the end of the script tag's id following its name.
+		/// </summary>
+		string PostString { get; set; }
 	}
 }
