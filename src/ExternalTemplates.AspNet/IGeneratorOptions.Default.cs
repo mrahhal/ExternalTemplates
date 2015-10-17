811bf92d39623562625941218c7d78fdcfe35df6
Mohammad Rahhal <m.r992@hotmail.com>
2015-10-17 09:43:25 +0300

    + IGeneratorOptions

---
+++ src/ExternalTemplates.AspNet/IGeneratorOptions.Default.cs
@@ -0,0 +1,20 @@
+﻿namespace ExternalTemplates
+{
+	/// <summary>
+	/// Default generator options.
+	/// </summary>
+	public class GeneratorOptions : IGeneratorOptions
+	{
+		/// <summary>
+		/// Gets the extension of the templates.
+		/// Default is ".tmpl.html".
+		/// </summary>
+		public string Extension { get; set; }
+
+		/// <summary>
+		/// Gets the path relative to the web root where the templates are stored.
+		/// Default is "/Content/templates".
+		/// </summary>
+		public string VirtualPath { get; set; }
+	}
+}
---
+++ src/ExternalTemplates.AspNet/IGeneratorOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace ExternalTemplates
+{
+	/// <summary>
+	/// Options for <see cref="IGenerator"/>.
+	/// </summary>
+	public interface IGeneratorOptions
+	{
+		/// <summary>
+		/// Gets the extension of the templates.
+		/// </summary>
+		string Extension { get; }
+
+		/// <summary>
+		/// Gets the path relative to the web root where the templates are stored.
+		/// </summary>
+		string VirtualPath { get; }
+	}
+}
