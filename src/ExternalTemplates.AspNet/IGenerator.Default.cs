3a2a2e6be1c482e037641e95dabefdcb35096f90
Mohammad Rahhal <m.r992@hotmail.com>
2015-10-17 10:11:38 +0300

    + IGenerator

---
+++ src/ExternalTemplates.AspNet/IGenerator.Default.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Dnx.Runtime;
+
+namespace ExternalTemplates
+{
+	/// <summary>
+	/// Default generator.
+	/// </summary>
+	public class Generator : IGenerator
+	{
+		private IApplicationEnvironment _appEnvironment;
+		private IGeneratorOptions _options;
+		private ICoreGenerator _coreGenerator;
+
+		public Generator(
+			IApplicationEnvironment appEnvironment,
+			IGeneratorOptions options,
+			ICoreGenerator coreGenerator)
+		{
+			_appEnvironment = appEnvironment;
+			_options = options;
+			_coreGenerator = coreGenerator;
+		}
+
+		/// <summary>
+		/// Generates the templates.
+		/// </summary>
+		/// <returns>
+		/// The generated combination of script tags from the external templates.
+		/// </returns>
+		public HtmlString Generate()
+		{
+			var templatesDirectory = GetTemplatesDirectory();
+			var templateFiles = GetTemplateFiles(templatesDirectory);
+
+			var sb = new StringBuilder();
+			foreach (var file in templateFiles)
+			{
+				var fileName = Path.GetFileName(file);
+				var templateName = fileName.Remove(fileName.Length - _options.Extension.Length);
+				sb.Append(
+					_coreGenerator.GenerateScriptTagFromStream(new FileStream(file, FileMode.Open, FileAccess.Read),
+					templateName));
+			}
+
+			return new HtmlString(sb.ToString());
+		}
+
+		private string GetTemplatesDirectory()
+		{
+			return Path.Combine(
+				_appEnvironment.ApplicationBasePath,
+				_options.VirtualPath);
+		}
+
+		private string[] GetTemplateFiles(string directory)
+		{
+			return Directory.EnumerateFiles(directory)
+				.Where(f => f.EndsWith(_options.Extension))
+				.ToArray();
+		}
+	}
+}
---
+++ src/ExternalTemplates.AspNet/IGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Mvc.Rendering;
+
+namespace ExternalTemplates
+{
+	/// <summary>
+	/// Finds external templates and generates a combination of script tags from them.
+	/// </summary>
+	public interface IGenerator
+	{
+		/// <summary>
+		/// Generates the templates.
+		/// </summary>
+		/// <returns>
+		/// The generated combination of script tags from the external templates.
+		/// </returns>
+		HtmlString Generate();
+	}
+}
