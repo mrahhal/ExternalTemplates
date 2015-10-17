fde47de2a44c9becfc6ef45b56f475b0d77930b0
Mohammad Rahhal <m.r992@hotmail.com>
2015-10-17 10:57:25 +0300

    Add the defaults for GeneratorOptions

--- src/ExternalTemplates.AspNet/IGeneratorOptions.Default.cs
+++ src/ExternalTemplates.AspNet/IGeneratorOptions.Default.cs
@@ -11,18 +11,18 @@ namespace ExternalTemplates
 		/// Gets the path relative to the web root where the templates are stored.
 		/// Default is "/Content/templates".
 		/// </summary>
-		public string VirtualPath { get; set; }
+		public string VirtualPath { get; set; } = "/Content/templates";
 
 		/// <summary>
 		/// Gets the extension of the templates.
 		/// Default is ".tmpl.html".
 		/// </summary>
-		public string Extension { get; set; }
+		public string Extension { get; set; } = ".tmpl.html";
 
 		/// <summary>
 		/// Gets the post string to add to the end of the script tag's id following its name.
 		/// Default is "-tmpl".
 		/// </summary>
-		public string PostString { get; set; }
+		public string PostString { get; set; } = "-tmpl";
 	}
 }
